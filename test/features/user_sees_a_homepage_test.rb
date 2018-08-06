9671ee72acedb0903ef1117f748c6cc0c8ba9ae2
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-08-06 11:34:44 -0600

    Step 2

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -4,8 +4,14 @@ class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
-    save_and_open_page
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
+  end
+
+  def test_user_gets_error_message_when_visiting_page_that_does_not_exist
+    visit '/somepage'
+
+    assert page.has_content?("Page not found.")
+    assert_equal 404, page.status_code
   end
 end
